47d49627efa8fc329ebf651b115ae056a77ca8a0
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2024-05-23 17:31:25 +0000

    fix: Improved and corrected loading of node configuration

--- app/src/utils/node.ts
+++ app/src/utils/node.ts
@@ -3,14 +3,56 @@ import * as path from 'path';
 import config from './configuration';
 import { Logger } from '@utils/logger';
 
+// Defaults values for node configuration
+const DATACENTER_GIGABYTE_PRICES="52573ibc/31FEE1A2A9F9C01113F90BD0BBCCE8FD6BBB8585FAF109A2101827DD1D5B95B8,9204ibc/A8C2D23A1E6F95DA4E48BA349667E322BD7A6C996D8A4AAE8BA72E190F3D1477,1180852ibc/B1C0DDB14F25279A2026BC8794E12B259F8BDA546A3C5132CCAEE4431CE36783,122740ibc/ED07A3391A112B175915CD8FAF43A2DA8E4790EDE12566649D0C2F97716B8518,15342624udvpn";
+const DATACENTER_HOURLY_PRICES="18480ibc/31FEE1A2A9F9C01113F90BD0BBCCE8FD6BBB8585FAF109A2101827DD1D5B95B8,770ibc/A8C2D23A1E6F95DA4E48BA349667E322BD7A6C996D8A4AAE8BA72E190F3D1477,1871892ibc/B1C0DDB14F25279A2026BC8794E12B259F8BDA546A3C5132CCAEE4431CE36783,18897ibc/ED07A3391A112B175915CD8FAF43A2DA8E4790EDE12566649D0C2F97716B8518,4160000udvpn";
+
+export interface NodeConfigData
+{
+	[key: string]: string | number | boolean | undefined;
+	moniker: string;
+	chain_id: string;
+	node_ip: string;
+	node_type: string;
+	node_port: number;
+	vpn_port: number;
+	backend: string;
+	handshake: boolean;
+	wallet_name: string;
+	max_peers: number;
+	gas: string;
+	gas_adjustment: string;
+	gas_prices: string;
+	node_location: string;
+	gigabyte_prices: string;
+	hourly_prices: string;
+}
+
 class NodeManager
 {
 	private static instance: NodeManager;
-	private config: ConfigData;
+	private nodeConfig: NodeConfigData = {
+		moniker: '',
+		chain_id: '',
+		node_ip: '',
+		node_type: '',
+		node_port: 0,
+		vpn_port: 0,
+		backend: '',
+		handshake: false,
+		wallet_name: '',
+		max_peers: 0,
+		gas: '',
+		gas_adjustment: '',
+		gas_prices: '',
+		node_location: '',
+		gigabyte_prices: '',
+		hourly_prices: '',
+	};
 	
 	private constructor()
 	{
-		this.config = config;
+		this.loadNodeConfig();
 	}
 	
 	/**
@@ -28,11 +70,11 @@ class NodeManager
 	
 	/**
 	 * Get current configuration
-	 * @returns ConfigData
+	 * @returns NodeConfigData
 	 */
-	public getConfig(): ConfigData
+	public getConfig(): NodeConfigData
 	{
-		return this.config;
+		return this.nodeConfig;
 	}
 	
 	/**
@@ -58,39 +100,59 @@ class NodeManager
 	 */
 	public loadNodeConfig(): void
 	{
-		const configFilePath = path.join(this.config.CONFIG_DIR, 'config.toml');
+		// Initialize node configuration file path
+		const configFilePath = path.join(config.CONFIG_DIR, 'config.toml');
+		// If the configuration files do not exist, do nothing
 		if (!this.isConfigFileAvailable(configFilePath))
 		{
 			Logger.info("Configuration files do not exist.");
-			return;
+			return ;
 		}
-		
-		Logger.info("Please wait while the configuration files are being loaded...");
 		
 		try
 		{
+			// Load configuration file content
 			const configFileContent = fs.readFileSync(configFilePath, 'utf8');
 			
-			this.config.NODE_MONIKER = this.extractConfigValue(configFileContent, 'moniker');
-			this.config.NODE_TYPE = this.extractConfigValue(configFileContent, 'type');
-			this.config.NODE_IP = this.extractConfigValue(configFileContent, 'remote_url').split('/')[2].split(':')[0];
-			this.config.NODE_PORT = this.extractConfigValue(configFileContent, 'listen_on').split(':')[1];
-			this.config.CHAIN_ID = this.extractConfigValue(configFileContent, 'id');
-			this.config.MAX_PEERS = this.extractConfigValue(configFileContent, 'max_peers');
-			this.config.BACKEND = this.extractConfigValue(configFileContent, 'backend');
-			this.config.WALLET_NAME = this.extractConfigValue(configFileContent, 'from');
-			this.config.HANDSHAKE_ENABLE = this.extractConfigValueInSection(configFileContent, 'handshake', 'enable');
-			const hourlyPrices = this.extractConfigValue(configFileContent, 'hourly_prices');
-			this.config.NODE_LOCATION = hourlyPrices === process.env.DATACENTER_HOURLY_PRICES ? 'datacenter' : hourlyPrices ? 'residential' : '';
-
-			Logger.info("Configuration files have been loaded.");
+			// Parse configuration file content
+			this.nodeConfig.moniker = this.extractConfigValue(configFileContent, 'moniker');
+			this.nodeConfig.node_type = this.extractConfigValue(configFileContent, 'type');
+			this.nodeConfig.node_ip = this.extractConfigValue(configFileContent, 'remote_url').split('/')[2].split(':')[0];
+			this.nodeConfig.node_port = parseInt(this.extractConfigValue(configFileContent, 'listen_on').split(':')[1]);
+			this.nodeConfig.chain_id = this.extractConfigValue(configFileContent, 'id');
+			this.nodeConfig.max_peers = parseInt(this.extractConfigValue(configFileContent, 'max_peers'));
+			this.nodeConfig.backend = this.extractConfigValue(configFileContent, 'backend');
+			this.nodeConfig.wallet_name = this.extractConfigValue(configFileContent, 'from');
+			this.nodeConfig.handshake = this.extractConfigValueInSection(configFileContent, 'handshake', 'enable') === 'true';
+			this.nodeConfig.gas = this.extractConfigValue(configFileContent, 'gas');
+			this.nodeConfig.gas_adjustment = this.extractConfigValue(configFileContent, 'gas_adjustment');
+			this.nodeConfig.gas_prices = this.extractConfigValue(configFileContent, 'gas_prices');
+			this.nodeConfig.gigabyte_prices = this.extractConfigValue(configFileContent, 'gigabyte_prices');
+			this.nodeConfig.hourly_prices = this.extractConfigValue(configFileContent, 'hourly_prices');
+			this.nodeConfig.node_location = this.nodeConfig.hourly_prices === DATACENTER_HOURLY_PRICES ? 'datacenter' : this.nodeConfig.hourly_prices ? 'residential' : '';
+			
+			// Load WireGuard configuration file content
+			if(this.nodeConfig.node_type === 'wireguard')
+			{
+				const wireguardConfigPath = path.join(config.CONFIG_DIR, 'wireguard.toml');
+				const wireguardConfigContent = fs.readFileSync(wireguardConfigPath, 'utf8');
+				this.nodeConfig.vpn_port = parseInt(this.extractConfigValue(wireguardConfigContent, 'listen_port'));
+			}
+			// Load V2Ray configuration file content
+			else if(this.nodeConfig.node_type === 'v2ray')
+			{
+				const v2rayConfigPath = path.join(config.CONFIG_DIR, 'v2ray.toml');
+				const v2rayConfigContent = fs.readFileSync(v2rayConfigPath, 'utf8');
+				this.nodeConfig.vpn_port = parseInt(this.extractConfigValue(v2rayConfigContent, 'listen_port'));
+			}
+			
 		}
 		catch (error)
 		{
 			Logger.error(`Error loading node configuration: ${error}`);
 		}
 	}
-
+	
 	/**
 	 * Extract configuration value from the content
 	 * @param content - File content
@@ -99,8 +161,17 @@ class NodeManager
 	 */
 	private extractConfigValue(content: string, key: string): string
 	{
-		const regex = new RegExp(`^${key}\\s*=\\s*"(.*)"`, 'm');
+		const regex = new RegExp(`^${key}\\s*=\\s*"?([^"\\r\\n]*)"?`, 'm');
 		const match = content.match(regex);
+		
+		if(key === 'handshake')
+		{
+			console.log(content, key)
+			console.log(regex);
+			console.log('match='+match[1]);
+			process.exit(1);
+		}
+		// Return the value if found, otherwise an empty string
 		return match ? match[1].trim() : '';
 	}
 	
@@ -113,11 +184,14 @@ class NodeManager
 	 */
 	private extractConfigValueInSection(content: string, section: string, key: string): string
 	{
+		// Create a regex to match the section
 		const sectionRegex = new RegExp(`\\[${section}\\]([\\s\\S]*?)\\[`, 'm');
 		const sectionMatch = content.match(sectionRegex);
+		// If the section is found, extract the key value
 		if(sectionMatch)
 		{
-			const keyRegex = new RegExp(`^${key}\\s*=\\s*"(.*)"`, 'm');
+			// Create a regex to match the key
+			const keyRegex = new RegExp(`^${key}\\s*=\\s*"?([^"\\r\\n]*)"?`, 'm');
 			const keyMatch = sectionMatch[1].match(keyRegex);
 			return keyMatch ? keyMatch[1].trim() : '';
 		}
@@ -129,43 +203,31 @@ class NodeManager
 	 */
 	public refreshConfigFiles(): void
 	{
-		const configFilePath = path.join(this.config.CONFIG_DIR, 'config.toml');
-
+		const configFilePath = path.join(config.CONFIG_DIR, 'config.toml');
+		
 		try
 		{
-			this.updateConfigValue(configFilePath, 'moniker', this.config.NODE_MONIKER);
-			this.updateConfigValue(configFilePath, 'id', this.config.CHAIN_ID);
-			this.updateConfigValue(configFilePath, 'rpc_addresses', this.config.RPC_ADDRESSES.replace(/\//g, '\\/'));
-			this.updateConfigValue(configFilePath, 'type', this.config.NODE_TYPE);
-			this.updateConfigValue(configFilePath, 'listen_on', `0.0.0.0:${this.config.NODE_PORT}`);
-			this.updateConfigValue(configFilePath, 'remote_url', `https://${this.config.NODE_IP}:${this.config.NODE_PORT}`);
-			this.updateConfigValue(configFilePath, 'backend', this.config.BACKEND);
-			this.updateConfigValueInSection(configFilePath, 'handshake', 'enable', this.config.HANDSHAKE_ENABLE);
-			this.updateConfigValue(configFilePath, 'max_peers', this.config.MAX_PEERS);
-			this.updateConfigValue(configFilePath, 'gas', this.config.GAS);
-			this.updateConfigValue(configFilePath, 'gas_adjustment', this.config.GAS_ADJUSTMENT);
-			this.updateConfigValue(configFilePath, 'gas_price', this.config.GAS_PRICE);
+			this.updateConfigValue(configFilePath, 'moniker', this.nodeConfig.moniker);
+			this.updateConfigValue(configFilePath, 'id', this.nodeConfig.chain_id);
+			this.updateConfigValue(configFilePath, 'type', this.nodeConfig.node_type);
+			this.updateConfigValue(configFilePath, 'listen_on', `0.0.0.0:${this.nodeConfig.node_port}`);
+			this.updateConfigValue(configFilePath, 'remote_url', `https://${this.nodeConfig.node_ip}:${this.nodeConfig.node_port}`);
+			this.updateConfigValue(configFilePath, 'backend', this.nodeConfig.backend);
+			this.updateConfigValueInSection(configFilePath, 'handshake', 'enable', this.nodeConfig.handshake ? 'true' : 'false');
+			this.updateConfigValue(configFilePath, 'max_peers', this.nodeConfig.max_peers);
+			this.updateConfigValue(configFilePath, 'gas', this.nodeConfig.gas);
+			this.updateConfigValue(configFilePath, 'gas_adjustment', this.nodeConfig.gas_adjustment);
+			this.updateConfigValue(configFilePath, 'gas_prices', this.nodeConfig.gas_prices);
 			
-			if(this.config.NODE_LOCATION === 'residential')
+			if(this.nodeConfig.node_type === 'wireguard')
 			{
-				this.updateConfigValue(configFilePath, 'gigabyte_prices', this.config.RESIDENTIAL_GIGABYTE_PRICES.replace(/\//g, '\\/'));
-				this.updateConfigValue(configFilePath, 'hourly_prices', this.config.RESIDENTIAL_HOURLY_PRICES.replace(/\//g, '\\/'));
+				const wireguardConfigPath = path.join(config.CONFIG_DIR, 'wireguard.toml');
+				this.updateConfigValue(wireguardConfigPath, 'listen_port', this.nodeConfig.vpn_port);
 			}
-			else
+			else if(this.nodeConfig.node_type === 'v2ray')
 			{
-				this.updateConfigValue(configFilePath, 'gigabyte_prices', this.config.DATACENTER_GIGABYTE_PRICES.replace(/\//g, '\\/'));
-				this.updateConfigValue(configFilePath, 'hourly_prices', this.config.DATACENTER_HOURLY_PRICES.replace(/\//g, '\\/'));
-			}
-			
-			if(this.config.NODE_TYPE === 'wireguard')
-			{
-				const wireguardConfigPath = path.join(this.config.CONFIG_DIR, 'wireguard.toml');
-				this.updateConfigValue(wireguardConfigPath, 'listen_port', this.config.WIREGUARD_PORT);
-			}
-			else if(this.config.NODE_TYPE === 'v2ray')
-			{
-				const v2rayConfigPath = path.join(this.config.CONFIG_DIR, 'v2ray.toml');
-				this.updateConfigValue(v2rayConfigPath, 'listen_port', this.config.V2RAY_PORT);
+				const v2rayConfigPath = path.join(config.CONFIG_DIR, 'v2ray.toml');
+				this.updateConfigValue(v2rayConfigPath, 'listen_port', this.nodeConfig.vpn_port);
 			}
 			
 			Logger.info("Configuration files have been refreshed.");
@@ -182,7 +244,7 @@ class NodeManager
 	 * @param key - Configuration key
 	 * @param value - New value
 	 */
-	private updateConfigValue(filePath: string, key: string, value: string): void
+	private updateConfigValue(filePath: string, key: string, value: string | number): void
 	{
 		const regex = new RegExp(`^(${key}\\s*=\\s*).*`, 'm');
 		const content = fs.readFileSync(filePath, 'utf8');
@@ -213,7 +275,7 @@ const nodeManager = NodeManager.getInstance();
 export default nodeManager;
 
 // Exporter des fonctions utilitaires
-export const nodeConfig = (): ConfigData => nodeManager.getConfig();
-export const isNodeConfigFileAvailable = (): boolean => nodeManager.isConfigFileAvailable(path.join(nodeManager.getConfig().CONFIG_DIR, 'config.toml'));
-export const isWireguardConfigFileAvailable = (): boolean => nodeManager.isConfigFileAvailable(path.join(nodeManager.getConfig().CONFIG_DIR, 'wireguard.toml'));
-export const isV2RayConfigFileAvailable = (): boolean => nodeManager.isConfigFileAvailable(path.join(nodeManager.getConfig().CONFIG_DIR, 'v2ray.toml'));
+export const nodeConfig = (): NodeConfigData => nodeManager.getConfig();
+export const isNodeConfigFileAvailable = (): boolean => nodeManager.isConfigFileAvailable(path.join(config.CONFIG_DIR, 'config.toml'));
+export const isWireguardConfigFileAvailable = (): boolean => nodeManager.isConfigFileAvailable(path.join(config.CONFIG_DIR, 'wireguard.toml'));
+export const isV2RayConfigFileAvailable = (): boolean => nodeManager.isConfigFileAvailable(path.join(config.CONFIG_DIR, 'v2ray.toml'));
