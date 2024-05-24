f2a5db10e002e1e7af77055072223e3bb0f11c25
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2024-05-24 16:08:54 +0000

    refactor: Start Docker container with passphrase if backend is file and wallet passphrase is provided

--- app/src/utils/docker.ts
+++ app/src/utils/docker.ts
@@ -1,6 +1,7 @@
 import Docker from 'dockerode';
 import config from './configuration';
 import { Logger } from './logger';
+import { nodeConfig, type NodeConfigData } from '@utils/node';
 
 const docker = new Docker();
 
@@ -108,4 +109,155 @@ export async function pullImage(): Promise<boolean>
 	}
 	
 	return false;
+}
+
+/**
+ * Start Docker container
+ * @param walletPassphrase string|null
+ * @returns boolean
+ */
+export async function containerStart(walletPassphrase: string | null = null): Promise<boolean>
+{
+	const configNode : NodeConfigData = nodeConfig();
+	
+	try
+	{
+		Logger.info(`Please wait while the dVPN node container is being started...`);
+		
+		const containerList = await docker.listContainers({ all: true });
+		const containerExists = containerList.some(container => container.Names.includes(`/${config.DOCKER_CONTAINER_NAME}`));
+		
+		if (containerExists)
+		{
+			const runningContainers = await docker.listContainers();
+			const containerRunning = runningContainers.some(container => container.Names.includes(`/${config.DOCKER_CONTAINER_NAME}`));
+			
+			if (!containerRunning)
+			{
+				if (configNode.backend === 'file' && walletPassphrase)
+				{
+					await startContainerWithPassphrase(config.DOCKER_CONTAINER_NAME, walletPassphrase);
+				}
+				else
+				{
+					await startContainerWithoutPassphrase(config.DOCKER_CONTAINER_NAME);
+				}
+				
+				Logger.info(`dVPN node container has been started successfully.`);
+			}
+			return true;
+		}
+		
+		const createOptions =
+		{
+			name: config.DOCKER_CONTAINER_NAME,
+			HostConfig: {
+				Binds: [
+					`${config.CONFIG_DIR}:/root/.sentinelnode`,
+					'/lib/modules:/lib/modules'
+				],
+				CapDrop: ['ALL'],
+				CapAdd: ['NET_ADMIN', 'NET_BIND_SERVICE', 'NET_RAW', 'SYS_MODULE'],
+				Sysctls: {
+					'net.ipv4.ip_forward': '1',
+					'net.ipv6.conf.all.disable_ipv6': '0',
+					'net.ipv6.conf.all.forwarding': '1',
+					'net.ipv6.conf.default.forwarding': '1'
+				},
+				PortBindings: {
+					[`${configNode.node_port}/tcp`]: [{ HostPort: `${configNode.node_port}` }]
+				}
+			}
+		};
+		
+		// Add port bindings for WireGuard or V2Ray
+		if (configNode.node_type === 'wireguard')
+		{
+			createOptions.HostConfig.PortBindings[`${configNode.vpn_port}/udp`] = [{ HostPort: `${configNode.vpn_port}` }];
+		}
+		else if (configNode.nodeType === 'v2ray')
+		{
+			createOptions.HostConfig.PortBindings[`${configNode.vpn_port}/tcp`] = [{ HostPort: `${configNode.vpn_port}` }];
+		}
+		else
+		{
+			Logger.error(`Invalid node type or missing port.`);
+			return false;
+		}
+		
+		if (configNode.backend === 'file' && walletPassphrase)
+		{
+			await startContainerWithPassphrase(config.DOCKER_CONTAINER_NAME, walletPassphrase, createOptions);
+		}
+		else
+		{
+			await docker.createContainer({
+				...createOptions,
+				Image: config.DOCKER_CONTAINER_NAME,
+				Cmd: ['process', 'start']
+			});
+			await startContainerWithoutPassphrase(config.DOCKER_CONTAINER_NAME);
+		}
+		
+		Logger.info(`dVPN node container has been started successfully.`);
+		return true;
+	}
+	catch (err)
+	{
+		if (err instanceof Error)
+			Logger.error(`Failed to start the dVPN node container: ${err.message}`);
+		else
+			Logger.error(`Failed to start the dVPN node container: ${String(err)}`);
+		
+		return false;
+	}
+}
+
+/**
+ * Start Docker container without passphrase
+ * @returns boolean
+ */
+async function startContainerWithoutPassphrase(containerName: string): Promise<void>
+{
+    await docker.getContainer(containerName).start();
+}
+
+/**
+ * Start Docker container with passphrase
+ * @param containerName string
+ * @param walletPassphrase string
+ * @param createOptions Docker.ContainerCreateOptions
+ * @returns void
+ */
+async function startContainerWithPassphrase(containerName: string, walletPassphrase: string, createOptions?: Docker.ContainerCreateOptions): Promise<void>
+{
+	const command = createOptions ? 'create' : 'start';
+	const options = createOptions ? createOptions : {};
+	
+	await new Promise<void>((resolve, reject) =>
+	{
+		const stream = docker.run(
+			containerName,
+			['bash', '-c', `echo '${walletPassphrase}' | docker ${command} -ai --detach-keys="ctrl-q" ${containerName}`],
+			process.stdout,
+			options,
+			(err: any) => {
+				if (err) return reject(err);
+				resolve();
+			}
+		);
+		
+		stream.on('data', (data: Buffer) => {
+			Logger.info(data.toString());
+		});
+		
+		stream.on('error', (err: any) => {
+			Logger.error(`Stream error: ${err}`);
+			reject(err);
+		});
+		
+		stream.on('end', () => {
+			resolve();
+		});
+	});
 }
