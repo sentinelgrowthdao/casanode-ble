3ddc1dc9c0f5e26b0bb2c2a3069a3037bf4e99c4
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2024-06-03 22:39:26 +0200

    feat: Add nodeConfigCharacteristic class for managing the node config characteristic in the daemon command

---
+++ app/src/characteristics/nodeConfig.ts
@@ -0,0 +1,73 @@
+import { createRequire } from 'module';
+
+import { Logger } from '@utils/logger';
+import nodeManager, { NodeConfigData } from '@utils/node';
+
+export class nodeConfigCharacteristic
+{
+	/**
+	 * Bleno instance
+	 * @type any
+	 */
+	private Bleno: any = undefined;
+	
+	/**
+	 * UUID of the characteristic
+	 * @type string
+	 */
+	private characteristicUuid: string = '';
+	
+	/**
+	 * Create a new instance of Characteristic
+	 */
+	constructor(uuid: string) 
+	{
+		const require = createRequire(import.meta.url);
+		this.Bleno = require('bleno');
+		this.characteristicUuid = uuid;
+	}
+	
+	/**
+	 * Create a new instance of nodeConfigCharacteristic
+	 */
+	public create()//: typeof Bleno.Characteristic 
+	{
+		if(this.Bleno === undefined)
+			return null;
+		
+		return new this.Bleno.Characteristic({
+			uuid: this.characteristicUuid,
+			properties: ['write'],
+			onWriteRequest: this.onWriteRequest.bind(this),
+		});
+	}
+	
+	/**
+	 * Called when the characteristic is written
+	 * @param data Buffer
+	 * @param offset number
+	 * @param withoutResponse boolean
+	 * @param callback (result: number) => void
+	 * @returns void
+	 */
+	public onWriteRequest(data: Buffer, offset: number, withoutResponse: boolean, callback: (result: number) => void)
+	{
+		// Get the value from the buffer
+		const value = data.toString('utf-8').trim();
+		
+		// Check if the value is invalid
+		if (value !== 'apply') 
+		{
+			Logger.error('Invalid value received via Bluetooth for "moniker".');
+			callback(this.Bleno.Characteristic.RESULT_INVALID_ATTRIBUTE_LENGTH);
+			return;
+		}
+		
+		// Update the configuration
+		nodeManager.refreshConfigFiles();
+		
+		// Notify the subscriber if the value is set
+		callback(this.Bleno.Characteristic.RESULT_SUCCESS);
+		Logger.info(`Parameter "moniker" updated via Bluetooth to: ${value}`);
+	}
+}
