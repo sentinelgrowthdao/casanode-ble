b7d77af5e65576e665d0e7437ecdbd010ec44ee3
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2024-07-25 10:50:36 +0000

    refactor: Using the UUID variable in config

--- app/src/commands/daemon.ts
+++ app/src/commands/daemon.ts
@@ -9,6 +9,7 @@
 
 import { createRequire } from 'module';	// <- Used to import the Bleno module in the same way as require
 import { Logger } from '@utils/logger';
+import config from '@utils/configuration';
 
 import { loadingNodeInformations, loadingSystemInformations } from '@actions/startup';
 
@@ -45,40 +46,40 @@ import { NodeMnemonicCharacteristic } from '@/characteristics/nodeMnemonic';
 import { WalletActionsCharacteristic } from '@/characteristics/walletActions';
 import { NodeKeyringBackendCharacteristic } from '@/characteristics/nodeKeyringBackend';
 
-// TODO: Add the UUIDs for the BLE service and characteristics in the configuration file
-const NODE_BLE_UUID = '0000180d-0000-1000-8000-00805f9b34fb';
-const CHAR_HELLO_UUID = '0000180d-0000-1000-8000-00805f9b34fc';
-const CHAR_MONIKER_UUID = '0000180d-0000-1000-8000-00805f9b34fd';
-const CHAR_NODE_TYPE_UUID = '0000180d-0000-1000-8000-00805f9b34fe';
-const CHAR_NODE_IP_UUID = '0000180d-0000-1000-8000-00805f9b34ff';
-const CHAR_NODE_PORT_UUID = '0000180d-0000-1000-8000-00805f9b3500';
-const CHAR_VPN_TYPE_UUID = '0000180d-0000-1000-8000-00805f9b3501';
-const CHAR_VPN_PORT_UUID = '0000180d-0000-1000-8000-00805f9b3502';
-const CHAR_MAX_PEERS_UUID = '0000180d-0000-1000-8000-00805f9b3503';
-const CHAR_NODE_CONFIG_UUID = '0000180d-0000-1000-8000-00805f9b3504';
-const CHAR_NODE_LOCATION_UUID = '0000180d-0000-1000-8000-00805f9b3505';
-const CHAR_CERT_EXPIRITY_UUID = '0000180d-0000-1000-8000-00805f9b3506';
-const CHAR_BANDWIDTH_SPEED_UUID = '0000180d-0000-1000-8000-00805f9b3507';
-const CHAR_SYSTEM_UPTIME_UUID = '0000180d-0000-1000-8000-00805f9b3508';
-const CHAR_CASANODE_VERSION_UUID = '0000180d-0000-1000-8000-00805f9b3509';
-const CHAR_DOCKER_IMAGE_UUID = '0000180d-0000-1000-8000-00805f9b350a';
-const CHAR_SYSTEM_OS_UUID = '0000180d-0000-1000-8000-00805f9b350b';
-const CHAR_SYSTEM_ARCH_UUID = '0000180d-0000-1000-8000-00805f9b350c';
-const CHAR_SYSTEM_KERNEL_UUID = '0000180d-0000-1000-8000-00805f9b350d';
-const CHAR_NODE_PASSPHRASE_UUID = '0000180d-0000-1000-8000-00805f9b350e';
-const CHAR_PUBLIC_ADDRESS_UUID = '0000180d-0000-1000-8000-00805f9b350f';
-const CHAR_ADDRESS_NODE_UUID = '0000180d-0000-1000-8000-00805f9b3510';
-const CHAR_NODE_BALANCE_UUID = '0000180d-0000-1000-8000-00805f9b3511';
-const CHAR_NODE_STATUS_UUID = '0000180d-0000-1000-8000-00805f9b3512';
-const CHAR_CHECK_INSTALL_UUID = '0000180d-0000-1000-8000-00805f9b3513';
-const CHAR_INSTALL_IMAGE_UUID = '0000180d-0000-1000-8000-00805f9b3514';
-const CHAR_INSTALL_CONFIGS_UUID = '0000180d-0000-1000-8000-00805f9b3515';
-const CHAR_NODE_ACTIONS_UUID = '0000180d-0000-1000-8000-00805f9b3516';
-const CHAR_SYSTEM_ACTIONS_UUID = '0000180d-0000-1000-8000-00805f9b3517';
-const CHAR_CERTIFICATE_ACTIONS_UUID = '0000180d-0000-1000-8000-00805f9b3518';
-const CHAR_WALLET_MNEMONIC_UUID = '0000180d-0000-1000-8000-00805f9b3519';
-const CHAR_WALLET_ACTIONS_UUID = '0000180d-0000-1000-8000-00805f9b351a';
-const CHAR_NODE_KEYRING_BACKEND_UUID = '0000180d-0000-1000-8000-00805f9b351b';
+// Bluetooth UUIDs
+const NODE_BLE_UUID = '00805f9b34fb';
+const CHAR_HELLO_UUID = '00805f9b34fc';
+const CHAR_MONIKER_UUID = '00805f9b34fd';
+const CHAR_NODE_TYPE_UUID = '00805f9b34fe';
+const CHAR_NODE_IP_UUID = '00805f9b34ff';
+const CHAR_NODE_PORT_UUID = '00805f9b3500';
+const CHAR_VPN_TYPE_UUID = '00805f9b3501';
+const CHAR_VPN_PORT_UUID = '00805f9b3502';
+const CHAR_MAX_PEERS_UUID = '00805f9b3503';
+const CHAR_NODE_CONFIG_UUID = '00805f9b3504';
+const CHAR_NODE_LOCATION_UUID = '00805f9b3505';
+const CHAR_CERT_EXPIRITY_UUID = '00805f9b3506';
+const CHAR_BANDWIDTH_SPEED_UUID = '00805f9b3507';
+const CHAR_SYSTEM_UPTIME_UUID = '00805f9b3508';
+const CHAR_CASANODE_VERSION_UUID = '00805f9b3509';
+const CHAR_DOCKER_IMAGE_UUID = '00805f9b350a';
+const CHAR_SYSTEM_OS_UUID = '00805f9b350b';
+const CHAR_SYSTEM_ARCH_UUID = '00805f9b350c';
+const CHAR_SYSTEM_KERNEL_UUID = '00805f9b350d';
+const CHAR_NODE_PASSPHRASE_UUID = '00805f9b350e';
+const CHAR_PUBLIC_ADDRESS_UUID = '00805f9b350f';
+const CHAR_ADDRESS_NODE_UUID = '00805f9b3510';
+const CHAR_NODE_BALANCE_UUID = '00805f9b3511';
+const CHAR_NODE_STATUS_UUID = '00805f9b3512';
+const CHAR_CHECK_INSTALL_UUID = '00805f9b3513';
+const CHAR_INSTALL_IMAGE_UUID = '00805f9b3514';
+const CHAR_INSTALL_CONFIGS_UUID = '00805f9b3515';
+const CHAR_NODE_ACTIONS_UUID = '00805f9b3516';
+const CHAR_SYSTEM_ACTIONS_UUID = '00805f9b3517';
+const CHAR_CERTIFICATE_ACTIONS_UUID = '00805f9b3518';
+const CHAR_WALLET_MNEMONIC_UUID = '00805f9b3519';
+const CHAR_WALLET_ACTIONS_UUID = '00805f9b351a';
+const CHAR_NODE_KEYRING_BACKEND_UUID = '00805f9b351b';
 
 export const daemonCommand = async () =>
 {
@@ -97,40 +98,40 @@ export const daemonCommand = async () =>
 	
 	// Create the primary service with the specified UUID and characteristics
 	const service = new bleno.PrimaryService({
-		uuid: NODE_BLE_UUID,
+		uuid: `${config.BLE_UUID}-${NODE_BLE_UUID}`,
 		characteristics: [
-			HelloCharacteristic.create(CHAR_HELLO_UUID),
-			new MonikerCharacteristic(CHAR_MONIKER_UUID).create(),
-			new NodeTypeCharacteristic(CHAR_NODE_TYPE_UUID).create(),
-			new NodeIpCharacteristic(CHAR_NODE_IP_UUID).create(),
-			new NodePortCharacteristic(CHAR_NODE_PORT_UUID).create(),
-			new VpnTypeCharacteristic(CHAR_VPN_TYPE_UUID).create(),
-			new VpnPortCharacteristic(CHAR_VPN_PORT_UUID).create(),
-			new MaxPeersCharacteristic(CHAR_MAX_PEERS_UUID).create(),
-			new NodeConfigCharacteristic(CHAR_NODE_CONFIG_UUID).create(),
-			new NodeLocationCharacteristic(CHAR_NODE_LOCATION_UUID).create(),
-			new CertExpirityCharacteristic(CHAR_CERT_EXPIRITY_UUID).create(),
-			new BandwidthSpeedCharacteristic(CHAR_BANDWIDTH_SPEED_UUID).create(),
-			new SystemUptimeCharacteristic(CHAR_SYSTEM_UPTIME_UUID).create(),
-			new CasanodeVersionCharacteristic(CHAR_CASANODE_VERSION_UUID).create(),
-			new DockerImageCharacteristic(CHAR_DOCKER_IMAGE_UUID).create(),
-			new SystemArchCharacteristic(CHAR_SYSTEM_ARCH_UUID).create(),
-			new SystemOsCharacteristic(CHAR_SYSTEM_OS_UUID).create(),
-			new SystemKernelCharacteristic(CHAR_SYSTEM_KERNEL_UUID).create(),
-			new NodePassphraseCharacteristic(CHAR_NODE_PASSPHRASE_UUID).create(),
-			new PublicAddressCharacteristic(CHAR_PUBLIC_ADDRESS_UUID).create(),
-			new NodeAddressCharacteristic(CHAR_ADDRESS_NODE_UUID).create(),
-			new NodeBalanceCharacteristic(CHAR_NODE_BALANCE_UUID).create(),
-			new NodeStatusCharacteristic(CHAR_NODE_STATUS_UUID).create(),
-			new CheckInstallationCharacteristic(CHAR_CHECK_INSTALL_UUID).create(),
-			new InstallDockerImageCharacteristic(CHAR_INSTALL_IMAGE_UUID).create(),
-			new InstallConfigsCharacteristic(CHAR_INSTALL_CONFIGS_UUID).create(),
-			new NodeActionsCharacteristic(CHAR_NODE_ACTIONS_UUID).create(),
-			new SystemActionsCharacteristic(CHAR_SYSTEM_ACTIONS_UUID).create(),
-			new CertificateActionsCharacteristic(CHAR_CERTIFICATE_ACTIONS_UUID).create(),
-			new NodeMnemonicCharacteristic(CHAR_WALLET_MNEMONIC_UUID).create(),
-			new WalletActionsCharacteristic(CHAR_WALLET_ACTIONS_UUID).create(),
-			new NodeKeyringBackendCharacteristic(CHAR_NODE_KEYRING_BACKEND_UUID).create(),
+			HelloCharacteristic.create(`${config.BLE_UUID}-${CHAR_HELLO_UUID}`),
+			new MonikerCharacteristic(`${config.BLE_UUID}-${CHAR_MONIKER_UUID}`).create(),
+			new NodeTypeCharacteristic(`${config.BLE_UUID}-${CHAR_NODE_TYPE_UUID}`).create(),
+			new NodeIpCharacteristic(`${config.BLE_UUID}-${CHAR_NODE_IP_UUID}`).create(),
+			new NodePortCharacteristic(`${config.BLE_UUID}-${CHAR_NODE_PORT_UUID}`).create(),
+			new VpnTypeCharacteristic(`${config.BLE_UUID}-${CHAR_VPN_TYPE_UUID}`).create(),
+			new VpnPortCharacteristic(`${config.BLE_UUID}-${CHAR_VPN_PORT_UUID}`).create(),
+			new MaxPeersCharacteristic(`${config.BLE_UUID}-${CHAR_MAX_PEERS_UUID}`).create(),
+			new NodeConfigCharacteristic(`${config.BLE_UUID}-${CHAR_NODE_CONFIG_UUID}`).create(),
+			new NodeLocationCharacteristic(`${config.BLE_UUID}-${CHAR_NODE_LOCATION_UUID}`).create(),
+			new CertExpirityCharacteristic(`${config.BLE_UUID}-${CHAR_CERT_EXPIRITY_UUID}`).create(),
+			new BandwidthSpeedCharacteristic(`${config.BLE_UUID}-${CHAR_BANDWIDTH_SPEED_UUID}`).create(),
+			new SystemUptimeCharacteristic(`${config.BLE_UUID}-${CHAR_SYSTEM_UPTIME_UUID}`).create(),
+			new CasanodeVersionCharacteristic(`${config.BLE_UUID}-${CHAR_CASANODE_VERSION_UUID}`).create(),
+			new DockerImageCharacteristic(`${config.BLE_UUID}-${CHAR_DOCKER_IMAGE_UUID}`).create(),
+			new SystemArchCharacteristic(`${config.BLE_UUID}-${CHAR_SYSTEM_ARCH_UUID}`).create(),
+			new SystemOsCharacteristic(`${config.BLE_UUID}-${CHAR_SYSTEM_OS_UUID}`).create(),
+			new SystemKernelCharacteristic(`${config.BLE_UUID}-${CHAR_SYSTEM_KERNEL_UUID}`).create(),
+			new NodePassphraseCharacteristic(`${config.BLE_UUID}-${CHAR_NODE_PASSPHRASE_UUID}`).create(),
+			new PublicAddressCharacteristic(`${config.BLE_UUID}-${CHAR_PUBLIC_ADDRESS_UUID}`).create(),
+			new NodeAddressCharacteristic(`${config.BLE_UUID}-${CHAR_ADDRESS_NODE_UUID}`).create(),
+			new NodeBalanceCharacteristic(`${config.BLE_UUID}-${CHAR_NODE_BALANCE_UUID}`).create(),
+			new NodeStatusCharacteristic(`${config.BLE_UUID}-${CHAR_NODE_STATUS_UUID}`).create(),
+			new CheckInstallationCharacteristic(`${config.BLE_UUID}-${CHAR_CHECK_INSTALL_UUID}`).create(),
+			new InstallDockerImageCharacteristic(`${config.BLE_UUID}-${CHAR_INSTALL_IMAGE_UUID}`).create(),
+			new InstallConfigsCharacteristic(`${config.BLE_UUID}-${CHAR_INSTALL_CONFIGS_UUID}`).create(),
+			new NodeActionsCharacteristic(`${config.BLE_UUID}-${CHAR_NODE_ACTIONS_UUID}`).create(),
+			new SystemActionsCharacteristic(`${config.BLE_UUID}-${CHAR_SYSTEM_ACTIONS_UUID}`).create(),
+			new CertificateActionsCharacteristic(`${config.BLE_UUID}-${CHAR_CERTIFICATE_ACTIONS_UUID}`).create(),
+			new NodeMnemonicCharacteristic(`${config.BLE_UUID}-${CHAR_WALLET_MNEMONIC_UUID}`).create(),
+			new WalletActionsCharacteristic(`${config.BLE_UUID}-${CHAR_WALLET_ACTIONS_UUID}`).create(),
+			new NodeKeyringBackendCharacteristic(`${config.BLE_UUID}-${CHAR_NODE_KEYRING_BACKEND_UUID}`).create(),
 		]
 	});
 	
