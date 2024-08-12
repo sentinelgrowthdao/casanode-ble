ff7ff69766cbaf6aba35e2e62bab0f764798c327
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2024-08-12 23:36:45 +0200

    fix: Docker installation modified to extend timeout to 5 minutes

--- app/src/characteristics/installDockerImage.ts
+++ app/src/characteristics/installDockerImage.ts
@@ -3,6 +3,13 @@ import { createRequire } from 'module';
 import { Logger } from '@utils/logger';
 import { imagePull } from '@utils/docker';
 
+enum InstallStatus {
+	NOT_STARTED = 0,
+	IN_PROGRESS = 1,
+	COMPLETED = 2,
+	ERROR = -1
+}
+
 export class InstallDockerImageCharacteristic
 {
 	/**
@@ -18,9 +25,15 @@ export class InstallDockerImageCharacteristic
 	private characteristicUuid: string = '';
 	
 	/**
+	 * Installation status
+	 * @type InstallStatus
+	 */
+	private installStatus: InstallStatus = InstallStatus.NOT_STARTED;
+	
+	/**
 	 * Create a new instance of Characteristic
 	 */
-	constructor(uuid: string) 
+	constructor(uuid: string)
 	{
 		const require = createRequire(import.meta.url);
 		this.Bleno = require('bleno');
@@ -37,26 +50,60 @@ export class InstallDockerImageCharacteristic
 		
 		return new this.Bleno.Characteristic({
 			uuid: this.characteristicUuid,
-			properties: ['read'],
+			properties: ['read', 'write'],
 			onReadRequest: this.onReadRequest.bind(this),
+			onWriteRequest: this.onWriteRequest.bind(this),
 		});
 	}
-	
+
 	/**
 	 * Called when the characteristic is read
 	 * @param offset number
 	 * @param callback (result: number, data: Buffer) => void
 	 * @returns void
 	 */
-	public onReadRequest(offset: number, callback: (result: number, data: Buffer) => void) 
+	public onReadRequest(offset: number, callback: (result: number, data: Buffer) => void)
 	{
-		imagePull().then((status: boolean) =>
+		let response;
+		switch(this.installStatus)
 		{
-			// Return the value to the subscriber
-			callback(this.Bleno.Characteristic.RESULT_SUCCESS, Buffer.from(status ? '1' : '0'));
-		}).catch((error: any) => {
-			Logger.error(`Error while reading the installation status: ${error}`);
-			callback(this.Bleno.Characteristic.RESULT_UNLIKELY_ERROR, Buffer.from('0'));
+			case InstallStatus.NOT_STARTED: response = '0'; break;
+			case InstallStatus.IN_PROGRESS: response = '1'; break;
+			case InstallStatus.COMPLETED: response = '2'; break;
+			case InstallStatus.ERROR: response = '-1'; break;
+		}
+		
+		Logger.info(`Install Docker Image status: ${response}`);
+		callback(this.Bleno.Characteristic.RESULT_SUCCESS, Buffer.from(response));
+	}
+	
+	// Start the installation process with a write request
+	public onWriteRequest(data: Buffer, offset: number, withoutResponse: boolean, callback: (result: number) => void)
+	{
+		if(this.installStatus === InstallStatus.IN_PROGRESS)
+		{
+			Logger.error('Installation already in progress');
+			callback(this.Bleno.Characteristic.RESULT_UNLIKELY_ERROR);
+			return;
+		}
+		
+		// Start the installation process
+		this.installStatus = InstallStatus.IN_PROGRESS;
+		// Send a response to the client
+		callback(this.Bleno.Characteristic.RESULT_SUCCESS);
+		
+		Logger.info('Starting Docker image installation');
+		// Start the installation process
+		imagePull()
+		.then((status: boolean) =>
+		{
+			Logger.info(`Docker installation status: ${status}`);
+			this.installStatus = status ? InstallStatus.COMPLETED : InstallStatus.ERROR;
+		})
+		.catch((error: any) =>
+		{
+			Logger.error(`Error while installing Docker image: ${error}`);
+			this.installStatus = InstallStatus.ERROR;
 		});
 	}
 }
