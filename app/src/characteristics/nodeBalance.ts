30ef9ac7fe9b072a576abec3390a29e17f96f6c6
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2024-08-13 00:37:58 +0200

    fix: node balance modified to support a waiting time of 3 minutes

--- app/src/characteristics/nodeBalance.ts
+++ app/src/characteristics/nodeBalance.ts
@@ -1,8 +1,14 @@
 import { createRequire } from 'module';
-
 import { Logger } from '@utils/logger';
 import nodeManager from '@utils/node';
 import { walletBalance } from '@utils/node';
+
+enum BalanceStatus {
+	NOT_STARTED = 0,
+	IN_PROGRESS = 1,
+	COMPLETED = 2,
+	ERROR = -1
+}
 
 export class NodeBalanceCharacteristic
 {
@@ -19,6 +25,18 @@ export class NodeBalanceCharacteristic
 	private characteristicUuid: string = '';
 	
 	/**
+	 * Balance retrieval status
+	 * @type BalanceStatus
+	 */
+	private balanceStatus: BalanceStatus = BalanceStatus.NOT_STARTED;
+	
+	/**
+	 * Wallet balance value
+	 * @type string
+	 */
+	private balanceValue: string = '0 DVPN';
+	
+	/**
 	 * Create a new instance of Characteristic
 	 */
 	constructor(private uuid: string) 
@@ -31,15 +49,16 @@ export class NodeBalanceCharacteristic
 	/**
 	 * Create a new instance of NodeBalanceCharacteristic
 	 */
-	public create()//: typeof Bleno.Characteristic 
+	public create() 
 	{
 		if(this.Bleno === undefined)
 			return null;
 		
 		return new this.Bleno.Characteristic({
 			uuid: this.characteristicUuid,
-			properties: ['read'],
+			properties: ['read', 'write'],
 			onReadRequest: this.onReadRequest.bind(this),
+			onWriteRequest: this.onWriteRequest.bind(this),
 		});
 	}
 	
@@ -49,27 +68,65 @@ export class NodeBalanceCharacteristic
 	 * @param callback (result: number, data: Buffer) => void
 	 * @returns void
 	 */
-	public onReadRequest(offset: number, callback: (result: number, data: Buffer) => void) 
+	public onReadRequest(offset: number, callback: (result: number, data: Buffer) => void)
 	{
-		const publicAddress = nodeManager.getConfig().walletPublicAddress;
-		// If the wallet public address is unavailable
-		if(publicAddress === '')
+		let response;
+		switch(this.balanceStatus)
 		{
-			callback(this.Bleno.Characteristic.RESULT_SUCCESS, Buffer.from('0 DVPN'));
+			case BalanceStatus.NOT_STARTED: response = '0'; break;
+			case BalanceStatus.IN_PROGRESS: response = '1'; break;
+			case BalanceStatus.COMPLETED: response = this.balanceValue; break;
+			case BalanceStatus.ERROR: response = '-1'; break;
+		}
+		
+		Logger.info(`Node balance status: ${response}`);
+		callback(this.Bleno.Characteristic.RESULT_SUCCESS, Buffer.from(response));
+	}
+	
+	/**
+	 * Called when the characteristic is written to start the balance retrieval process
+	 * @param data Buffer
+	 * @param offset number
+	 * @param withoutResponse boolean
+	 * @param callback (result: number) => void
+	 * @returns void
+	 */
+	public onWriteRequest(data: Buffer, offset: number, withoutResponse: boolean, callback: (result: number) => void) 
+	{
+		if(this.balanceStatus === BalanceStatus.IN_PROGRESS)
+		{
+			Logger.error('Balance retrieval already in progress');
+			callback(this.Bleno.Characteristic.RESULT_UNLIKELY_ERROR);
 			return;
 		}
 		
-		// Get the wallet balance
-		walletBalance(publicAddress).then((balance) =>
+		this.balanceStatus = BalanceStatus.IN_PROGRESS;
+		callback(this.Bleno.Characteristic.RESULT_SUCCESS);
+		
+		Logger.info('Starting wallet balance retrieval');
+		
+		const publicAddress = nodeManager.getConfig().walletPublicAddress;
+		
+		if (publicAddress === '')
 		{
-			// Get the value from the configuration
-			const value = `${balance.amount} ${balance.denom}`;
-			// Return the value to the subscriber
-			callback(this.Bleno.Characteristic.RESULT_SUCCESS, Buffer.from(value));
-		}).catch((error) =>
+			this.balanceStatus = BalanceStatus.ERROR;
+			this.balanceValue = '0 DVPN';
+			Logger.error('Public address is missing');
+			return;
+		}
+		
+		walletBalance(publicAddress)
+		.then((balance) =>
 		{
-			Logger.error(`Error while getting the wallet balance: ${error}`);
-			callback(this.Bleno.Characteristic.RESULT_UNLIKELY_ERROR, Buffer.from('0 DVPN'));
+			this.balanceValue = `${balance.amount} ${balance.denom}`;
+			this.balanceStatus = BalanceStatus.COMPLETED;
+			Logger.info(`Wallet balance retrieved successfully: ${this.balanceValue}`);
+		})
+		.catch((error: any) =>
+		{
+			Logger.error(`Error while retrieving wallet balance: ${error}`);
+			this.balanceStatus = BalanceStatus.ERROR;
+			this.balanceValue = '0 DVPN';
 		});
 	}
 }
