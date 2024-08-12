0c69b911b516d3968459835d5d343aa9276408e3
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2024-08-13 01:21:56 +0200

    fix: SSL certificate regeneration modified to allow a Bluetooth timeout of 3 minutes.

--- app/src/characteristics/certificateActions.ts
+++ app/src/characteristics/certificateActions.ts
@@ -1,9 +1,13 @@
 import { createRequire } from 'module';
-import { exec } from 'child_process';
-import * as os from 'os';
-
 import { Logger } from '@utils/logger';
 import { certificateGenerate } from '@utils/certificate';
+
+enum CertificateStatus {
+	NOT_STARTED = 0,
+	IN_PROGRESS = 1,
+	COMPLETED = 2,
+	ERROR = -1
+}
 
 export class CertificateActionsCharacteristic
 {
@@ -20,6 +24,12 @@ export class CertificateActionsCharacteristic
 	private characteristicUuid: string = '';
 	
 	/**
+	 * Certificate renewal status
+	 * @type CertificateStatus
+	 */
+	private certStatus: CertificateStatus = CertificateStatus.NOT_STARTED;
+	
+	/**
 	 * Create a new instance of Characteristic
 	 */
 	constructor(private uuid: string) 
@@ -30,18 +40,51 @@ export class CertificateActionsCharacteristic
 	}
 	
 	/**
-	 * Create a new instance of NodePortCharacteristic
+	 * Create a new instance of CertificateActionsCharacteristic
 	 */
-	public create()//: typeof Bleno.Characteristic 
+	public create() 
 	{
 		if(this.Bleno === undefined)
 			return null;
 		
 		return new this.Bleno.Characteristic({
 			uuid: this.characteristicUuid,
-			properties: ['write'],
+			properties: ['read', 'write'],
+			onReadRequest: this.onReadRequest.bind(this),
 			onWriteRequest: this.onWriteRequest.bind(this),
 		});
+	}
+	
+	/**
+	 * Called when the characteristic is read
+	 * @param offset number
+	 * @param callback (result: number, data: Buffer) => void
+	 * @returns void
+	 */
+	public onReadRequest(offset: number, callback: (result: number, data: Buffer) => void)
+	{
+		let response;
+		switch(this.certStatus)
+		{
+			case CertificateStatus.NOT_STARTED:
+				response = '0'; 
+				break;
+			case CertificateStatus.IN_PROGRESS:
+				response = '1'; 
+				break;
+			case CertificateStatus.COMPLETED:
+				response = '2'; 
+				break;
+			case CertificateStatus.ERROR:
+				response = '-1'; 
+				break;
+			default:
+				response = '0'; 
+				break;
+		}
+		
+		Logger.info(`Certificate action status: ${response}`);
+		callback(this.Bleno.Characteristic.RESULT_SUCCESS, Buffer.from(response));
 	}
 	
 	/**
@@ -57,25 +100,37 @@ export class CertificateActionsCharacteristic
 		// Get the value from the buffer
 		const action = data.toString('utf-8').trim();
 		
+		if(this.certStatus === CertificateStatus.IN_PROGRESS)
+		{
+			Logger.error('Certificate renewal already in progress');
+			callback(this.Bleno.Characteristic.RESULT_UNLIKELY_ERROR);
+			return;
+		}
+		
 		if(action === 'renew')
 		{
+			this.certStatus = CertificateStatus.IN_PROGRESS;
+			callback(this.Bleno.Characteristic.RESULT_SUCCESS);
+			
+			Logger.info('Starting certificate renewal process.');
+			
 			certificateGenerate().then((success: boolean) =>
 			{
 				if(success)
 				{
+					this.certStatus = CertificateStatus.COMPLETED;
 					Logger.info('Certificate renewed successfully.');
-					callback(this.Bleno.Characteristic.RESULT_SUCCESS);
 				}
 				else
 				{
+					this.certStatus = CertificateStatus.ERROR;
 					Logger.error('Failed to renew certificate.');
-					callback(this.Bleno.Characteristic.RESULT_UNLIKELY_ERROR);
 				}
 			})
 			.catch(error =>
 			{
-				Logger.error(`Error renewing certificate and restarting Docker container: ${error}`);
-				callback(this.Bleno.Characteristic.RESULT_UNLIKELY_ERROR);
+				this.certStatus = CertificateStatus.ERROR;
+				Logger.error(`Error renewing certificate: ${error}`);
 			});
 		}
 		else
